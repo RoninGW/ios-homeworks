0e578969cbdd7740dbf53c30bc59cd1a80953ab1
Sergey <moonangeal@mail.ru>
2022-04-27 22:07:21 +0300

    add task 1.3

---
+++ Navigation/Navigation/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Navigation
+//
+//  Created by Сергей  Быков on 17.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainUITabBarController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Сергей  Быков on 25.04.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+            super.viewDidLoad()
+        view.backgroundColor = .yellow
+            title = "Профиль"
+    }
+    
+}
